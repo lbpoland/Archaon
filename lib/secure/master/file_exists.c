f4c5fb5466a90263699fe11c83412fbc15781c5d
lbpoland <https://github.com/lbpoland/Archaon.git>
2025-03-06 17:27:33 +0000

    /secure/master/file_exists.c

--- lib/secure/master/file_exists.c
+++ lib/secure/master/file_exists.c
@@ -1,14 +1,27 @@
-/*  -*- LPC -*-  */
-/*
- * $Locker:  $
- * $Id: file_exists.c,v 1.1 1998/01/06 05:12:03 ceres Exp $
- * $Log: file_exists.c,v $
- * Revision 1.1  1998/01/06 05:12:03  ceres
- * Initial revision
- * 
-*/
-int file_exists( string path ) {
-  if ( file_size( path ) < 0 )
+// -*- LPC -*-
+// /secure/master/file_exists.c
+// Recoded for FluffOS v2019+, themed for Forgotten Realms, integrating Discworld mechanics
+// Last updated: March 07, 2025
+
+#include <config.h>
+#include <log.h>
+
+// New Features:
+// - file_exists: Checks if a tome exists in the Weave (Input: string path, Output: int) - Added by Grok3
+// Usage Notes: Call file_exists("/path/to/tome") to check existence; returns 1 if found, 0 if not.
+int file_exists(string path) {
+    int size;
+
+    if (!path || !stringp(path)) {
+        LOG_HANDLER->log("ERROR", "Invalid path sought in the Weave at " + ctime(time()) + "\n");
+        return 0;
+    }
+
+    size = file_size(path);
+    if (size > 0 || size == -2) { // File or directory exists
+        LOG_HANDLER->log("EVENT", "Tome " + path + " found in the Weave at " + ctime(time()) + "\n");
+        return 1;
+    }
+    LOG_HANDLER->log("EVENT", "Tome " + path + " not found in the Weave at " + ctime(time()) + "\n");
     return 0;
-  return 1;
-} /* file_exists() */
+}
